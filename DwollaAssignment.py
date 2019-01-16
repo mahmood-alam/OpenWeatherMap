ddefc33791763b43bd73b332b72a58488d7e3412
mahmood-alam <m.alam099@gmail.com>
2019-01-16 17:40:43 -0600

    Update DwollaAssignment.py

--- DwollaAssignment.py
+++ DwollaAssignment.py
@@ -6,26 +6,31 @@ api = "bc620ead2d18122876f00d05d2993999"
 # Base url
 base = "http://api.openweathermap.org/data/2.5/weather?"
 
-# Prompt city (only grab first word inputed)
-city = raw_input("Where are you: ").split(' ', 1)[0]
+def test_City():
+    # Prompt city (only grab first word inputed)
+    city = raw_input("Where are you: ").split(' ', 1)[0]
 
-# Api request url
-url = base + "appid=" + api + "&q=" + city
+    # Retrieve weather data
+    weather = findTemp(city)
 
-# Units in Farenheight
-params = {'units': 'imperial'}
+    # Ensure city was retrieved
+    assert weather["cod"] != "404", "City not found"
 
-# Get city data
-response = requests.get(url, params=params)
-
-# Convert json format into python format
-data = response.json()
-
-# If city was retrieved
-if data["cod"] != "404":
-    print(data["main"])
-    temp = str(data["main"]["temp"])
+    # Print city temperature
+    temp = str(weather["main"]["temp"])
     print(city+" weather:\n" + temp + " degrees Farenheit")
 
-else:
-    print("Could not retrieve city")
+def findTemp(city):
+    # Api request url
+    url = base + "appid=" + api + "&q=" + city
+
+    # Units in Farenheight
+    params = {'units': 'imperial'}
+
+    # Get city data
+    response = requests.get(url, params=params)
+
+    # Convert json format into python format
+    return response.json()
+
+test_City()
